93389d3cccd39797de8af99d4e0596ca5959ec81
Weiming.Wei <Weiming.wei@feisu.com>
2021-05-31 18:42:47 +0800

    update

--- src/main/java/com/example/myapplication/Application.java
+++ src/main/java/com/example/myapplication/Application.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 //@EnableAutoConfiguration
 //@ComponentScan
 @RestController
-public class Application {
+public class Application implements CommandLineRunner {
+    private String show;
 
     @RequestMapping("/")
     String home(){
-        return "Hello Application!";
+        return show;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    //CommandLineRunner接口，run方法在SpringApplication.run程序启动之前执行
+    @Override
+    public void run(String... strings) throws Exception {
+        this.show = "Hello Applications!";
+    }
 }
